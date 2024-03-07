ea4df5c9894565533ae7ee3c8581b83de6e33f7b
SKTT1Ryze <ccc@neos.moe>
2024-03-07 17:07:43 +0800

    update virtual function

--- include/virtualFunction.h
+++ include/virtualFunction.h
@@ -1,1 +1,11 @@
+namespace vfunction {
+struct Base {
+  virtual int f();
+};
+
+struct Derived : Base {
+  int f() override;  // `override` is optional
+};
+}  // namespace vfunction
+
 void executeVirtualFuncton();
--- src/virtualFunction.cc
+++ src/virtualFunction.cc
@@ -1,3 +1,29 @@
 #include "../include/virtualFunction.h"
 
-void executeVirtualFuncton() {}
+#include <cassert>
+
+using namespace vfunction;
+
+int Base::f() { return 1; }
+
+int Derived::f() { return 2; }
+
+void executeVirtualFuncton() {
+  Base b;
+  Derived d;
+
+  // virtual function call through reference
+  Base& br = b;  // the type of br is Base&
+  Base& dr = d;  // the type of dr is Base& as well
+  assert(br.f() == 1);
+  assert(dr.f() == 2);
+
+  // virtual function call through pointer
+  Base* bp = &b;  // the type of bp is Base*
+  Base* dp = &d;  // the type of dp is Base* as well
+  assert(bp->f() == 1);
+  assert(dp->f() == 2);
+
+  assert(br.Base::f() == 1);
+  assert(dr.Base::f() == 1);
+}
