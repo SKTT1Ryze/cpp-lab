c1e94068477cd6a119df7e5ecaa926fd6cb553d1
SKTT1Ryze <ccc@neos.moe>
2024-03-07 18:11:48 +0800

    update abstract class

--- include/abstractClass.h
+++ include/abstractClass.h
@@ -1,1 +1,16 @@
+namespace abstract {
+struct Abstract {
+  virtual void f() = 0;  // pure virtual
+};                       // "Abstract" is abstract
+
+struct Concrete : Abstract {
+  void f() override {}  // non-pure virtual
+  virtual void g() {}   // non-pure virtual
+};                      // "Concrete" is non-abstract
+
+struct Abstract2 : Concrete {
+  void g() override = 0;  // pure virtual overrider
+};                        // "Abstract2" is abstract
+}  // namespace abstract
+
 void executeAbstractClass();
--- src/abstractClass.cc
+++ src/abstractClass.cc
@@ -1,3 +1,11 @@
 #include "../include/abstractClass.h"
 
-void executeAbstractClass() {}
+using namespace abstract;
+
+void executeAbstractClass() {
+  // Abstract a;   // Error: abstract class
+  Concrete b;       // OK
+  Abstract& a = b;  // OK to reference abstract base
+  a.f();            // virtual dispatch to Concrete::f()
+  // Abstract2 a2; // Error: abstract class (final overrider of g() is pure)
+}
